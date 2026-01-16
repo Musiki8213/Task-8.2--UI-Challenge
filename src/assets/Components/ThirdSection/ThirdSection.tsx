a63acfa8d691ffcef5cd7e2b029bc930c895107a
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-16 19:52:34 +0200

    style: fix third section alignment on small screens

--- src/assets/Components/ThirdSection/ThirdSection.tsx
+++ src/assets/Components/ThirdSection/ThirdSection.tsx
@@ -1,34 +1,30 @@
-
 import "./ThirdSection.css";
 
 export const ThirdSection = () => {
   return (
     <div className="third-section">
-      <div>
-        <div style={{ overflow: "hidden" }}>
+      <div className="third-section-content">
+        <div className="third-section-images">
           <img
-            src="public/bg-pattern-circles.svg"
-            alt="icon-supervisor"
+            src="/bg-pattern-circles.svg"
+            alt="Background pattern"
             className="circle-img"
-          style={{ overflow: "hidden"}}/>
+          />
+          <img
+            src="/illustration-phones.svg"
+            alt="Phones illustration"
+            className="phone-img"
+          />
         </div>
-        <img
-          src="public/illustration-phones.svg"
-          alt="icon-supervisor"
-          className="phone-img"
-        />
-      </div>
-      <div className="text">
-        <h3>State of the Art Infrastructure</h3>
-        <p>
-          With reliability and speed in mind, worldwide data centers provide the
-          <br />
-          backbone for ultra-fast connectivity. This ensures your site will load
-          <br />
-          instantly, no matter where your readers are, keeping your site
-          <br />
-          competitive.
-        </p>
+        <div className="third-section-text">
+          <h3>State of the Art Infrastructure</h3>
+          <p>
+            With reliability and speed in mind, worldwide data centers provide the
+            backbone for ultra-fast connectivity. This ensures your site will load
+            instantly, no matter where your readers are, keeping your site
+            competitive.
+          </p>
+        </div>
       </div>
     </div>
   );
