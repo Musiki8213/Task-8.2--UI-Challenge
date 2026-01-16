947847ee6a1ea9fb50faa29bc2720b5378cec6ec
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-16 19:51:31 +0200

    style: improve app layout and mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ import {SecondSection} from "./assets/Components/SecondSection/SecondSection";
 import { ThirdSection } from './assets/Components/ThirdSection/ThirdSection';       
 import {FourthSection} from "./assets/Components/FourthSection/FourthSection";  
 import {Footer} from "./assets/Components/Footer/Footer";
-import Sidebar from './assets/Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-  < Navbar/>
+      <Navbar/>
       <SecondSection/>
       <ThirdSection/>
       <FourthSection/>
       <Footer/> 
-      <Sidebar isOpen={false} onClose={() => {}} />
     </div>
   )
 }
