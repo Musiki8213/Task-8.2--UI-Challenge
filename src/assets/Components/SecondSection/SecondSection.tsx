bb779654fe42cdf3535e35f1d045969c7b8051e7
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-16 19:52:21 +0200

    style: improve second section spacing and responsiveness

--- src/assets/Components/SecondSection/SecondSection.tsx
+++ src/assets/Components/SecondSection/SecondSection.tsx
@@ -1,42 +1,44 @@
-
 import "./SecondSection.css";
 
 export const SecondSection = () => {
   return (
     <section className="second-section">
-      <h2>Designed for the future</h2>
+      <h2 className="section-title">Designed for the future</h2>
 
       <div className="second-section-content">
         <div className="second-section-texts">
-          <h3>Introducing an extensivle editor</h3>
-          <p>
-            Blogr features an exceedingly intuitive interface which lets you
-            focus <br />
-            on one thing: creating content. The editor supports management of{" "}
-            <br />
-            multiple blogs and allows easy manipulation of embeds such as
-            images, <br />
-            videos, and Markdown. Extensibility with plugins and themes provide{" "}
-            <br />
-            easy ways to add functionality or change the looks of a blog.
-          </p>
+          <div className="text-block">
+            <h3>Introducing an extensible editor</h3>
+            <p>
+              Blogr features an exceedingly intuitive interface which lets you
+              focus on one thing: creating content. The editor supports management of
+              multiple blogs and allows easy manipulation of embeds such as
+              images, videos, and Markdown. Extensibility with plugins and themes provide
+              easy ways to add functionality or change the looks of a blog.
+            </p>
+          </div>
 
-          <h3>Robust content management</h3>
-          <p>
-            Flexible content management enables users to easily move through{" "}
-            <br />
-            posts. Increase the usability of your blog by adding customized{" "}
-            <br />
-            categories, sections, format, or flow. With this functionality,
-            you’re in <br />
-            full control.
-          </p>
+          <div className="text-block">
+            <h3>Robust content management</h3>
+            <p>
+              Flexible content management enables users to easily move through
+              posts. Increase the usability of your blog by adding customized
+              categories, sections, format, or flow. With this functionality,
+              you're in full control.
+            </p>
+          </div>
         </div>
 
-        <div>
+        <div className="second-section-image">
           <img
-            src="public/illustration-editor-desktop.svg"
-            alt="desktopImage"
+            src="/illustration-editor-desktop.svg"
+            alt="Editor illustration"
+            className="desktop-image"
+          />
+          <img
+            src="/illustration-editor-mobile.svg"
+            alt="Editor illustration"
+            className="mobile-image"
           />
         </div>
       </div>
