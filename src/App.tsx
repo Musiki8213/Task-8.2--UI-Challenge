595549b7a5cdfc08986965e5ea9211d2ecf5a78a
Musiki8213 <sithomolamusiki@gmail.com>
2026-03-06 11:29:53 +0200

    Update App component to use new component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
-import {Navbar} from "./assets/Components/Navbar/Navbar";
-import {SecondSection} from "./assets/Components/SecondSection/SecondSection";
-import { ThirdSection } from './assets/Components/ThirdSection/ThirdSection';       
-import {FourthSection} from "./assets/Components/FourthSection/FourthSection";  
-import {Footer} from "./assets/Components/Footer/Footer";
+import {Navbar} from "./Components/Navbar/Navbar";
+import {SecondSection} from "./Components/SecondSection/SecondSection";
+import { ThirdSection } from './Components/ThirdSection/ThirdSection';       
+import {FourthSection} from "./Components/FourthSection/FourthSection";  
+import {Footer} from "./Components/Footer/Footer";
 
 function App() {
   return (
